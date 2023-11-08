70e5cd007c8a4548efb6cc9c79bc27d14a0cc1e9
Leo Barnett <leo.e.barnett@icloud.com>
2023-11-08 17:00:57 +0200

    Initial Commit

---
+++ RockScissorsPaper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RockScissorsPaper
+//
+//  Created by Leo  on 08.11.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RockScissorsPaper/RockScissorsPaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockScissorsPaperApp.swift
+//  RockScissorsPaper
+//
+//  Created by Leo  on 08.11.23.
+//
+
+import SwiftUI
+
+@main
+struct RockScissorsPaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
